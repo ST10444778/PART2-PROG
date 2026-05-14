778d7f5b7ea4773821d14cfa42dcd3b22fd06993
ST10444778 <160589968+ST10444778@users.noreply.github.com>
2026-05-14 22:51:53 +0200

    Created WinForms GUI layout

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CyberSecurityChatbotGUI2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(100, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(611, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CYBERSECURITY AWARENESS CHATBOT";
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.BackColor = System.Drawing.Color.White;
+            this.rtbChat.Location = new System.Drawing.Point(161, 89);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.ReadOnly = true;
+            this.rtbChat.Size = new System.Drawing.Size(460, 267);
+            this.rtbChat.TabIndex = 1;
+            this.rtbChat.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.LightBlue;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSend.Location = new System.Drawing.Point(545, 387);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(161, 389);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(100, 20);
+            this.txtUserInput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.rtbChat);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtUserInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyberSecurityChatbotGUI2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                SoundPlayer player = new SoundPlayer("greeting.wav");
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Audio file could not play.");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string userInput = txtUserInput.Text.ToLower();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                DisplayBotMessage("Please enter a message.");
+            }
+            else if (userInput.Contains("password"))
+            {
+                DisplayBotMessage("Use strong and unique passwords for every account.");
+            }
+            else if (userInput.Contains("phishing"))
+            {
+                DisplayBotMessage("Be careful of suspicious emails and unknown links.");
+            }
+            else if (userInput.Contains("privacy"))
+            {
+                DisplayBotMessage("Privacy helps protect your personal information online.");
+            }
+            else if (userInput.Contains("worried"))
+            {
+                DisplayBotMessage("It's understandable to feel worried about cybersecurity threats.");
+            }
+            else if (userInput.Contains("curious"))
+            {
+                DisplayBotMessage("Curiosity is great! Learning cybersecurity helps keep you safe online.");
+            }
+            else
+            {
+                DisplayBotMessage("I didn't understand that. Try asking something else.");
+            }
+
+            txtUserInput.Clear();
+        }
+        private void DisplayBotMessage(string message)
+        {
+            rtbChat.AppendText("Bot: " + message + "\n");
+        }
+    }
+}
